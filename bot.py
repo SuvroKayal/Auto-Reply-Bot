9f7b238b4cb48b10a1571ad354d9d41fea8c7480
Suvro Kayal <suvrokayal2005@gmail.com>
2026-09-13 20:35:22 +0530

    Modified bot.py and debug some errors

--- bot.py
+++ bot.py
@@ -1,43 +1,61 @@
-
 import pyautogui
 import time
 import pyperclip
 from gemini_ai import ask_suvro
 
-# Click the icon
+def is_last_message_from_sender(chat_log):
+    messages = chat_log.strip().split("] ")[-1]
+    sender = messages.split(":")[0].strip()
+
+    return sender != "Suvro"
+
+# Click the icon once
 pyautogui.click(847, 750)
-time.sleep(1)
+time.sleep(2)
 
-# Select the message text
-pyautogui.moveTo(483, 92)
-pyautogui.dragTo(1330, 642, duration=1, button="left")
-time.sleep(0.5)
 
-# Copy selected text
-pyautogui.hotkey("ctrl", "c")
-time.sleep(0.5)
+while True:
 
-# Get copied text
-text = pyperclip.paste()
+    # Select the message text
+    pyautogui.moveTo(483, 92)
+    pyautogui.dragTo(1330, 642, duration=1, button="left")
+    time.sleep(0.5)
 
-print("Received:", text)
+    # Copy selected text
+    pyautogui.hotkey("ctrl", "c")
+    time.sleep(0.5)
 
-# Generate Gemini response
-response = ask_suvro(text)
+    # Get copied text
+    text = pyperclip.paste()
 
-print("Response:", response)
+    print("Received:", text)
 
-# Copy response to clipboard
-pyperclip.copy(response)
+    # Deselect the selected text 
+    pyautogui.click(483, 92) 
+    time.sleep(0.5)
 
-# Click WhatsApp message box
-pyautogui.click(766, 689)
-time.sleep(0.5)
+    # Reply only if the last message is NOT from Suvro
+    if not is_last_message_from_sender(text):
+        time.sleep(2)
+        continue
 
-# Paste response
-pyautogui.hotkey("ctrl", "v")
-time.sleep(0.5)
+    # Generate Gemini response
+    response = ask_suvro(text)
 
-# Send message
-pyautogui.click(1331, 677)
+    print("Response:", response)
 
+    # Copy response to clipboard
+    pyperclip.copy(response)
+
+    # Click WhatsApp message box
+    pyautogui.click(725, 674)
+    time.sleep(0.5)
+
+    # Paste response
+    pyautogui.hotkey("ctrl", "v")
+    time.sleep(0.5)
+
+    # Send message
+    pyautogui.click(1331, 677)
+
+    time.sleep(20)
